dd6ce61cdcd45fc059e1ff1a18cf372622df52db
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-12-06 09:11:21 -0500

    Matriz aleatorias de la primera y segunda

--- suma.c
+++ suma.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-void sumamatriz(int fila, int columna, int matrizprimera[fila][columna], int matrizsegunda[fila][columna], int matrizresultante[fila][columna]);
+/*Funcion para las sumas*/
+void sumamatriz(int fila, int columna, int matrizprimera[fila][columna], int matrizsegunda[fila][columna], int matrizresultante[fila][columna])
+{
+}
 int main(int argc, char const *argv[])
 {
     int fila, columna;
@@ -8,5 +11,21 @@ int main(int argc, char const *argv[])
     printf("Ingrese el numero de columnas : ");
     scanf("%d", &columna);
     int matrizprimera[fila][columna], matrizsegunda[fila][columna], matrizresultante[fila][columna];
+    /*Matriz aleatorrias tanto para la primera y la segunda*/
+    for (int i = 0; i < fila; i++)
+    {
+        for (int j = 0; j < columna; j++)
+        {
+            matrizprimera[i][j] = rand() % 100 + 1;
+        }
+    }
+    for (int i = 0; i < fila; i++)
+    {
+        for (int j = 0; j < columna; j++)
+        {
+            matrizsegunda[i][j] = rand() % 100 + 1;
+        }
+    }
+
     return 0;
 }
