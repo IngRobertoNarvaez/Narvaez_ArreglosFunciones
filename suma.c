1d74948ea4244fafe3e273548f2e6ee3ea5541b9
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-12-06 09:00:06 -0500

    Funcion de la sumas

---
+++ suma.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void sumamatriz(int fila, int columna, int matrizprimera[fila][columna], int matrizsegunda[fila][columna], int matrizresultante[fila][columna]);
+int main(int argc, char const *argv[])
+{
+    int fila, columna;
+    printf("Ingrese el numero de filas : ");
+    scanf("%d", &fila);
+    printf("Ingrese el numero de columnas : ");
+    scanf("%d", &columna);
+    int matrizprimera[fila][columna], matrizsegunda[fila][columna], matrizresultante[fila][columna];
+    return 0;
+}
