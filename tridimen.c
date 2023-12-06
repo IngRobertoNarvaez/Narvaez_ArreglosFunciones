412f61876359a8eabb3555f15d2c0ed83c356210
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-12-06 11:33:08 -0500

    Funcion de inicio de las matrices

--- tridimen.c
+++ tridimen.c
@@ -21,5 +21,10 @@ int main()
     printf("\n");
     int matriz[kcaras][ifila][jcolumna];
     srand(time(NULL));
+    // Llamada a la función para inicializar las matrices
+    inicializarMatrices(kcaras, ifila, jcolumna, matriz);
+
+    // Llamada a la función para imprimir las matrices
+    imprimir(kcaras, ifila, jcolumna, matriz);
     return 0;
 }
