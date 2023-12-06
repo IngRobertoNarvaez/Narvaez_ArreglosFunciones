242e25537a1371636600b779e2d02a1ae1a894e7
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-12-06 11:44:34 -0500

    Final codigo

--- tridimen.c
+++ tridimen.c
@@ -3,9 +3,44 @@
 #include <time.h>
 
 // Prototipo de funciones
-void inicializarMatrices(int kcaras, int ifila, int jcolumna, int matriz[kcaras][ifila][jcolumna]);
-void imprimir(int kcaras, int ifila, int jcolumna, int matriz[kcaras][ifila][jcolumna]);
+void inicializarMatrices(int kcaras, int ifila, int jcolumna, int matriz[kcaras][ifila][jcolumna])
+{
+    for (int k = 0; k < kcaras; k++)
+    {
+        for (int i = 0; i < ifila; i++)
+        {
+            for (int j = 0; j < jcolumna; j++)
+            {
+                matriz[k][i][j] = 0;
+            }
+        }
+    }
 
+    // La matriz de numeros unos se iniciará aquí
+    for (int i = 0; i < ifila; i++)
+    {
+        for (int j = 0; j < jcolumna; j++)
+        {
+            matriz[kcaras - 1][i][j] = 1;
+        }
+    }
+}
+void imprimir(int kcaras, int ifila, int jcolumna, int matriz[kcaras][ifila][jcolumna])
+{
+    for (int k = 0; k < kcaras; k++)
+    {
+        printf("Matriz de cara %d:\n", k + 1);
+        for (int i = 0; i < ifila; i++)
+        {
+            for (int j = 0; j < jcolumna; j++)
+            {
+                printf("%d ", matriz[k][i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+}
 int main()
 {
     int ifila;
@@ -21,10 +56,10 @@ int main()
     printf("\n");
     int matriz[kcaras][ifila][jcolumna];
     srand(time(NULL));
-    // Llamada a la función para inicializar las matrices
+    // Llamada a la funcion para inicializar las matrices
     inicializarMatrices(kcaras, ifila, jcolumna, matriz);
 
-    // Llamada a la función para imprimir las matrices
+    // Llamada a la funcion para imprimir las matrices
     imprimir(kcaras, ifila, jcolumna, matriz);
     return 0;
 }
